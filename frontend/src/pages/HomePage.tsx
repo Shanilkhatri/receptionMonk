cce349415d434dbf6014551f2a6e42e663bb3606
Bhavana Awasthi <bhavana.awasthi@reak.in>
2023-10-16 06:01:13 +0000

    Reception Monk UI (#29)
    RECEPTION MONK UI
    
    > Please take a look Screenshot which is attached for all screen resolution.
    
    Reviewed-on: https://git.reak.in/REAK-Skunkworks/receptionmonk/pulls/29
    Co-authored-by: Bhavana Awasthi <bhavana.awasthi@reak.in>
    Co-committed-by: Bhavana Awasthi <bhavana.awasthi@reak.in>

---
+++ frontend/src/pages/HomePage.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+import Header from "../partials/Header";
+import Hero from "../components/Hero";
+import ContentOne from "../components/BlockContentOne";
+import ContentTwo from "../components/BlockContentTwo";
+import BlockPrice from "../components/PriceTable";
+import Footer from "../partials/Footer";
+
+function HomePage() {
+    return (
+        <div>
+            
+            <div className="bg-img">
+
+                {/* Header Section */}
+                <Header />     
+
+                {/* Hero Section */}
+                <Hero /> 
+
+                {/* Content Block One Section */}
+                <ContentOne />
+
+                {/* Content Block Two Section */}
+                <ContentTwo />
+
+            </div>              
+
+            <div>
+
+                {/* Price Section */}
+                <BlockPrice />
+
+            </div>   
+
+            <div>
+
+                {/* Footer Section */}
+                <Footer />
+            </div>
+
+        </div>    
+    );
+}
+export default HomePage;
