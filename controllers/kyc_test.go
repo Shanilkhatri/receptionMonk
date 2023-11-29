9ba29945a0347694f7c1a3adda8191f4180b6e2f
pratik.maheshwari <pratik.maheshwari@reak.in>
2023-11-29 14:37:33 +0530

    incorrect data test case

--- controllers/kyc_test.go
+++ controllers/kyc_test.go
@@ -83,9 +83,77 @@ func TestPutKycDetailsWithTypeUserAccessingAnotherUser(t *testing.T) {
 	}
 }
 
-// TEST #2 - PUT kyc with correct Struct and also correct Data
+// TEST #2 - PutKyc with incorrect parameters
+// -> user tries to put incorrect kyc data
+// -> we won't mock DB as we expect to get error unauhtorized access before the DB ops
+// expecting a 400
+func TestPutKycDetailsWithIncorrectData(t *testing.T) {
+	// parameters to be send on the basis of which we can filter orders and fetch
+	// instead of sending json data we have to send parametrs in query
+	queryParams := map[string]interface{}{
+		"doc_name":     22,
+		"doc_pic_name": "img",
+		"userid":       1,
+		"companyId":    1,
+	}
+	requestBody, err := json.Marshal(queryParams)
+	if err != nil {
+		panic(err)
+	}
+
+	// mocking token payload with userDetails as user
+	// haven't populated all the fields as they aren't required
+	var userdetails utility.UserDetails
+	userdetails.ID = 2                // userId #1 trying to access records of userId #2
+	userdetails.AccountType = "owner" // type set to user
+	userdetails.CompanyID = 2
+
+	// Mocking the utility functions that are used there
+	Helper = MockHelper{
+		MockCheckTokenPayloadAndReturnUserBool:    true,
+		MockCheckTokenPayloadAndReturnUserDetails: userdetails,
+	}
+	// // open Mock DB connection
+	mockDB, dbmock, err := sqlmock.New()
+	defer mockDB.Close()
+	sqlxDB := sqlx.NewDb(mockDB, "sqlmock")
+
+	// I have used mustBegin thats why I am using Expect begin
+	dbmock.ExpectBegin()
+
+	// Binding the DB Cursor to correct utility.Db
+	utility.Db = sqlxDB
+
+	// here we will prepare the url with parameters to pass to our request
+	// Create a mock Request
+	request := httptest.NewRequest(http.MethodPut, "/responses", bytes.NewBuffer(requestBody))
+	w := httptest.NewRecorder()
+	// Call your function with the mocks
+	// Call your function with the mocks
+	PutKycDetails(w, request)
+
+	// Read the response body into a byte slice
+	body, err := ioutil.ReadAll(w.Body)
+	if err != nil {
+		// here i am just logging the error
+		log.Println(err)
+	}
+
+	var data utility.AjaxResponce
+	// Unmarshal the JSON data into the struct
+	if err := json.Unmarshal(body, &data); err != nil {
+		// Handle the JSON unmarshaling error
+		log.Println("error", err)
+	}
+
+	if data.Message != "Please check all fields correctly and try again." && w.Result().StatusCode != 400 {
+		t.Errorf("Expected status code %d, got %d", http.StatusForbidden, w.Result().StatusCode)
+	}
+}
+
+// TEST #3 - PUT kyc with correct Struct and also correct Data
 // -> owner tries to add someone else's kyc of same company
-// -> for the second test we'll mock the user details from the token and make accountType : owner
+// -> for the third test we'll mock the user details from the token and make accountType : owner
 // -> we will mock DB as we expect to get result
 // expecting a 200
 func TestPutKycDetailsWithCorrectData(t *testing.T) {
